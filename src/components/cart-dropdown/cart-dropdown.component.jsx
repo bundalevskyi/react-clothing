b787ec21093db04d64cbf04f6c99ec183ab1f6d2
Alex <bundalevsky99@gmail.com>
2024-02-21 22:31:35 +0100

    added product logic

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,16 +1,29 @@
+import { useContext } from "react";
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
+import { CartContext } from "../../contexts/cart.context";
+import { useNavigate } from "react-router-dom";
 
 
 
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext)
+  const navigate = useNavigate()
+
+
+  const handleLink = () => {
+    navigate('/chekout')
+  }
     
 
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
-      <Button>GO TO CHECKOUT</Button>
+      <div className="cart-items">
+        {cartItems.map((item) => <CartItem key={item.id} cartItem={item}/> )}
+      </div>
+      <Button onClick={handleLink}>GO TO CHECKOUT</Button>
     </div>
   );
 };
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,21 +1,24 @@
-import React, { useContext } from "react";
-import { ProductContext } from "../../contexts/products.context";
+import React, { useContext, Fragment } from "react";
 import ProductCard from "../../components/product-card/product.card.component";
 import "./shop.styles.scss";
-
-
-
-
+import { CategoriesContext } from "../../contexts/categories.context";
 
 const Shop = () => {
-  const { products } = useContext(ProductContext);
+  const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <div className="products-container">
-      {products.map(( product ) => (
-        <ProductCard key={product.id} product={product} />
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => (
+        <Fragment key={title}>
+          <h2>{title}</h2>
+          <div className="products-container">
+            {categoriesMap[title].map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Fragment>
       ))}
-    </div>
+    </Fragment>
   );
 };
 
